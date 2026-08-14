1cc997ca4396de14f198456124c0436985c6ff7a
arthursswz <jaoenglish77@gmail.com>
2026-08-14 16:52:41 -0300

    fix: modified the atividade-de-nivelamento-resolvida

--- 4_periodo/estrutura-de-dados(WESLEY)/exercicios/atividade-de-nivelamento-resolvida.c
+++ 4_periodo/estrutura-de-dados(WESLEY)/exercicios/atividade-de-nivelamento-resolvida.c
@@ -16,42 +16,71 @@ typedef struct loja{
     int totalEstoque;
     float totalVendas;
     product *ptrProduto;
-}
+} store;
 
 //função responsável por armazenar os produtos
- void adicionarProdutos(produto produtos[], int *qtdcadastradas ){
-   int controledecadastros = 0
+ void adicionarProdutos(product produtos[], int *qtdcadastradas ){
+   
 
-   if(controledecadastros <100){}
-    do{
-        
-    controledecadastros++
+   if(*qtdcadastradas==0){
+    printf("nenhum produto foi cadastrado até o momento");
+   }
+
+   if(*qtdcadastradas<=100){
+     (*qtdcadastradas)++;      
      printf("Informe o codigo do produto");
-             scanf("%s", &produtos.codigo[qtdcadastradas + controledecadastros]);
+             scanf("%d", &produtos[*qtdcadastradas].codigo);
+            do{ 
+                for(int i=1;i<qtdcadastradas; i++){
+                    if(produtos[*qtdcadastradas].codigo == produtos[*i].codigo){
+                        printf("já existe um mesmo código no sistema, digite outro");
+                    } while(produto.[*qtdcadastradas].codigo == produto[*i].codigo);
+                }
+                { 
                printf("Informe o nome do produto");
-               scanf("%s", &produtos.nome[qtdcadastradas + controledecadastros]);
+               scanf("%s", &produtos[*qtdcadastradas].nome);
                 printf("Informe a marca do produto");
-                scanf("%s", &produtos.marca[qtdcadastradas + controledecadastros]);
+                scanf("%s", &produtos[*qtdcadastradas].marca);
                 printf("Informe o tempo de garantia do produto em meses");
-                scanf("%s", &produtos.garantia[qtdcadastradas + controledecadastros]);
+                scanf("%d", &produtos[*qtdcadastradas].garantia);
                 printf("Informe o preço do produto");
-                scanf("%s", &produtos.preco[qtdcadastradas + controledecadastros]);
+                scanf("%f", produtos[*qtdcadastradas].preco);
                 printf("Informe a quantidade disponivel do produto em estoque");
-                scanf("%s", &produtos.nome[qtdcadastradas + controledecadastros])
+                scanf("%s", produtos[*qtdcadastradas].quantidade);
+                 
 
-    }while(controledecadastros<=100) 
+    } else {
+        printf("limite excedido de cadastro de produtos");
+    }
 
  }
 
 
 
-main(){
+int main(){
   //vetor de produtos com capacidade máxima de armazenamento de 100 produtos    
   product Produtos[100];
   //varíavel responsável por controlar a quantidade cadastradas
-  int *qtdcadastradas, controledecadastros = 0;
+  int controledecadastros = 0;
+  int opcao;
 
-  qtdcadastradas = Produtos;
+  do{
+    printf("---------------------------");
+    printf("sistema de gerenciamento da loja");
+    printf("----------------------------");
+
+    printf("1 - adicionar produto");
+     printf("2 - listar produtos");
+      printf("3 - buscar produtos pelo codigo");
+        printf("4 - vender produto");
+        printf("5- para sair");
+        scanf("%d", &opcao);
+
+        switch (opcao){
+            case 1:
+            adicionarProdutos(Produtos,&controledecadastros);
+        }
+  } while(opcao!=5);
 
 
 
